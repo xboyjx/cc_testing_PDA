3a7bee824b4da525bcf129cf73792bee6578f5e0
Jacob <callaghanjacob@gmail.com>
2022-04-04 09:54:35 +0100

    test for check_for_ace function done and failed

---
+++ part_2_code/specs/card_game_tests.py
@@ -0,0 +1,13 @@
+import unittest
+from src.card import Card
+from src.card_game import CardGame
+
+class TestCardGame(unittest.TestCase):
+    def setUp(self):
+        self.card1 = Card("Hearts", 10)
+        self.card2 = Card("Spades", 8)
+        self.card3 = Card("Diamonds", 7)
+        self.cards = [self.card1, self.card2, self.card3]
+
+    def test_check_for_ace(self):
+        self.assertEqual(CardGame.check_for_ace(self.card1), False)
